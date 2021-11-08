f6ace02a6e05a481497b7477f5adfbe3d4485201
Ingo Albrecht <commit@promovicz.org>
2021-11-08 05:32:11 +0100

    Remove strcasecmp replacement

--- src/symbol.c
+++ src/symbol.c
@@ -42,28 +42,17 @@
 
 static Object intern_symbol (char *name);
 
-#ifdef NO_STRCASECMP
-int strcasecmp (unsigned char *s1, unsigned char *s2);
-
-#endif
-
 /* local data */
 
 /* If SYMTAB_SIZE is not a power of 2, see change required below. */
 #define SYMTAB_SIZE 1024
 struct symtab *symbol_table[SYMTAB_SIZE];
-unsigned char chartable[1 << sizeof (char) * 8];
 
 /* function definitions */
 
 void
 init_symbol_prims (void)
 {
-  int c;
-
-  for (c = 0; c < (1 << sizeof (char) * 8); c++) {
-    chartable[c] = tolower (c);
-  }
 }
 
 Object
@@ -100,17 +89,6 @@ make_setter_symbol (Object sym)
   return (make_symbol (name));
 }
 
-#ifdef NO_STRCASECMP
-int
-strcasecmp (unsigned char *s1, unsigned char *s2)
-{
-  while ((chartable[*s1] == chartable[*s2++])) {
-    if (!chartable[*s1++])
-      return 0;
-  }
-  return (chartable[*s1] - chartable[*--s2]);
-}
-#endif
 
 static Object
 intern_symbol (char *name)
