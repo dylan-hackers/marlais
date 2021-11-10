e5e82ba55d11784f2f013c33e94c59133f8ef6d1
Ingo Albrecht <commit@promovicz.org>
2021-11-10 03:57:11 +0100

    Formatting and comments

--- include/marlais/apply.h
+++ include/marlais/apply.h
@@ -9,16 +9,16 @@
 extern void marlais_register_apply (void);
 /* Create the default result value */
 extern Object marlais_default_result_value (void);
-/* Internal version of apply */
+/* Internal version of apply (real one is in eval.c) */
 extern Object marlais_apply_internal (Object fun, Object args);
-/* Internal version of apply */
+/* Apply a specific method */
 extern Object marlais_apply_method (Object meth,
-				    Object args,
-				    Object rest_methods,
-				    Object generic_apply);
-/* */
+                                    Object args,
+                                    Object rest_methods,
+                                    Object generic_apply);
+/* Construct and check return values */
 extern Object marlais_construct_return_values (Object ret,
-					       Object required_values,
-					       Object rest_values);
+                                               Object required_values,
+                                               Object rest_values);
 
 #endif
