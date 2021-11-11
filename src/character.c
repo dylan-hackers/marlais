5487dcb08cd1f57839c0f3290ec26286bec2a24f
Ingo Albrecht <commit@promovicz.org>
2021-11-11 10:11:36 +0100

    Implement some ctype primitives

--- src/character.c
+++ src/character.c
@@ -51,15 +51,41 @@ static Object character_cache[MARLAIS_CONFIG_CHARACTER_CACHE];
 
 static Object integer_to_character (Object i);
 static Object character_to_integer (Object ch);
+
 static Object character_to_lowercase (Object ch);
 static Object character_to_uppercase (Object ch);
+
+static Object character_alphabetic_p (Object ch);
+static Object character_alphanumeric_p (Object ch);
+static Object character_control_p (Object ch);
+static Object character_graphic_p (Object ch);
+static Object character_printable_p (Object ch);
+static Object character_whitespace_p (Object ch);
+static Object character_decimal_p (Object ch);
+static Object character_hexadecimal_p (Object ch);
+static Object character_octal_p (Object ch);
+static Object character_lowercase_p (Object ch);
+static Object character_uppercase_p (Object ch);
 
 static struct primitive char_prims[] =
 {
   {"%integer->character", prim_1, integer_to_character},
   {"%character->integer", prim_1, character_to_integer},
+
   {"%character-to-lowercase", prim_1, character_to_lowercase},
   {"%character-to-uppercase", prim_1, character_to_uppercase},
+
+  {"%character-alphabetic?", prim_1, character_alphabetic_p},
+  {"%character-alphanumeric?", prim_1, character_alphanumeric_p},
+  {"%character-control?", prim_1, character_control_p},
+  {"%character-graphic?", prim_1, character_graphic_p},
+  {"%character-printable?", prim_1, character_printable_p},
+  {"%character-whitespace?", prim_1, character_whitespace_p},
+  {"%character-decimal?", prim_1, character_decimal_p},
+  {"%character-hexadecimal?", prim_1, character_hexadecimal_p},
+  //{"%character-octal?", prim_1, character_octal_p},
+  {"%character-lowercase?", prim_1, character_lowercase_p},
+  {"%character-uppercase?", prim_1, character_uppercase_p},
 };
 
 /* Exported functions */
@@ -124,3 +150,20 @@ character_to_uppercase (Object ch)
 {
   return (marlais_make_character (toupper(CHARVAL(ch))));
 }
+
+#define DEFINE_CTYPE_PREDICATE(_NAME,_PRED)		\
+  static Object character_ ## _NAME ## _p (Object ch) { \
+    return (marlais_make_boolean(_PRED(CHARVAL(ch))));	\
+  }
+DEFINE_CTYPE_PREDICATE(alphabetic,   isalpha);
+DEFINE_CTYPE_PREDICATE(alphanumeric, isalnum);
+DEFINE_CTYPE_PREDICATE(control,      iscntrl);
+DEFINE_CTYPE_PREDICATE(graphic,      isgraph);
+DEFINE_CTYPE_PREDICATE(printable,    isprint);
+DEFINE_CTYPE_PREDICATE(whitespace,   isspace);
+DEFINE_CTYPE_PREDICATE(decimal,      isdigit);
+DEFINE_CTYPE_PREDICATE(hexadecimal,  isxdigit);
+//DEFINE_CTYPE_PREDICATE(octal,        isdigit); /* XXX wrong */
+DEFINE_CTYPE_PREDICATE(lowercase,    islower);
+DEFINE_CTYPE_PREDICATE(uppercase,    isupper);
+#undef DEFINE_CTYPE_PREDICATE
