9928529d43fd715d604703b6d8e52edcf05cccaf
Ingo Albrecht <commit@promovicz.org>
2021-11-11 13:11:47 +0100

    Move string/char expansion out of lexer

--- include/marlais/lexer.h
+++ include/marlais/lexer.h
@@ -14,4 +14,7 @@ extern void marlais_lexer_reset (void);
 extern void marlais_lexer_push_intermediate_words (Object begin_word);
 extern void marlais_lexer_pop_intermediate_words (void);
 
+extern Object marlais_lexer_expand_char (char *str);
+extern Object marlais_lexer_expand_string (char *str);
+
 #endif
--- src/lexer.c
+++ src/lexer.c
@@ -1,2 +1,82 @@
 
 #include <marlais/lexer.h>
+
+#include <marlais/alloc.h>
+#include <marlais/bytestring.h>
+#include <marlais/character.h>
+
+#include "parser.gen.h"
+#include "lexer.gen.h"
+
+/* Internal function declarations */
+
+static char expand_escaped_character (char ch);
+
+/* Exported functions */
+
+Object
+marlais_lexer_expand_char (char *str)
+{
+  size_t len = strlen(str);
+  char c = str[1];
+  if(c == '\\') {
+    c = expand_escaped_character(str[2]);
+  }
+  return marlais_make_character(c);
+}
+
+Object
+marlais_lexer_expand_string (char *str)
+{
+  char* backslash = strchr(str, '\\');
+  if (backslash) {
+    char* exp_str;
+    Object obj;
+
+    exp_str = marlais_allocate_strdup (str);
+    exp_str[0] = '\0';
+    while (backslash) {
+      backslash[0] = expand_escaped_character(backslash[1]);
+      backslash[1] = '\0';
+      strcat(exp_str, str);
+      str = backslash + 2;
+      backslash = strchr(str, '\\');
+    }
+    strcat(exp_str, str);
+
+    obj = marlais_allocate_object (ByteString, sizeof (struct byte_string));
+    BYTESTRSIZE(obj) = strlen (exp_str);
+    BYTESTRVAL(obj) = exp_str;
+    return (obj);
+  }
+  return marlais_make_bytestring(str);
+}
+
+/* Internal functions */
+
+static char
+expand_escaped_character (char ch)
+{
+  switch (ch) {
+  case 'a':
+    return '\a';
+  case 'b':
+    return '\b';
+  case 'f':
+    return '\f';
+  case 'n':
+    return '\n';
+  case 'r':
+    return '\r';
+  case 't':
+    return '\t';
+  case 'v':
+    return '\v';
+#if 0
+    /* we are not bold enough for this */
+  case '0':
+    return '0';
+#endif
+  }
+  return ch;
+}
