8785315e81d74ce2d1f71d24fe0e7c85adf22034
Ingo Albrecht <commit@promovicz.org>
2021-11-23 19:19:24 +0100

    Fix comments

--- include/marlais/config.h
+++ include/marlais/config.h
@@ -33,9 +33,9 @@
 /* Cache the first N wide characters */
 #define MARLAIS_CONFIG_WCHAR_CACHE 1024
 
-/* Enable wide-character support */
+/* Enable unicode-character support */
 #define MARLAIS_ENABLE_UCHAR
-/* Enable wide-character support */
+/* Enable unicode-character support */
 #define MARLAIS_CONFIG_UCHAR_CACHE 1024
 
 #define MAX_STRING_SIZE 10240
