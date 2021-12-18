8b0b4ce91ee8da260ad69d541abddb52783986c0
Ingo Albrecht <commit@promovicz.org>
2021-12-18 14:19:30 +0100

    Comments

--- include/marlais/bignum.h
+++ include/marlais/bignum.h
@@ -8,19 +8,19 @@ extern void marlais_initialize_gmp(void);
 /* Register gmp primitives */
 extern void marlais_register_gmp(void);
 
-/* Make an <mp-float> */
+/* Make an <big-float> */
 extern Object marlais_make_mpf_from_number(Object value);
-/* Make an <mp-float> from a byte string */
+/* Make an <big-float> from a byte string */
 extern Object marlais_make_mpf_from_string(const char *str, int base);
 
-/* Make an <mp-ratio> */
+/* Make an <big-ratio> */
 extern Object marlais_make_mpq_from_number(Object value);
-/* Make an <mp-ratio> from a byte string */
+/* Make an <big-ratio> from a byte string */
 extern Object marlais_make_mpq_from_string(const char *str, int base);
 
-/* Make an <mp-integer> */
+/* Make an <big-integer> */
 extern Object marlais_make_mpz_from_number(Object value);
-/* Make an <mp-integer> from a byte string */
+/* Make an <big-integer> from a byte string */
 extern Object marlais_make_mpz_from_string(const char *str, int base);
 
 #endif /* !MARLAIS_GMP_H */
