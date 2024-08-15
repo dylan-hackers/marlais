1e64b047071e275270722fe92fc80100d59fe183
Ingo Albrecht <commit@promovicz.org>
2024-08-15 18:41:35 +0200

    Add counted_by

--- include/marlais/core/compiler.h
+++ include/marlais/core/compiler.h
@@ -9,11 +9,14 @@
 #define MARLAIS_FUN_PURE     __attribute__((pure))
 #define MARLAIS_FUN_NORETURN __attribute__((noreturn))
 #define MARLAIS_FUN_NONNULL  __attribute__((nonnull))
+//#define __marlais_counted_by(_field) __attribute__((__counted_by__(_field)))
 #else
 #define MARLAIS_FUN_CONST
 #define MARLAIS_FUN_PURE
 #define MARLAIS_FUN_NORETURN
 #define MARLAIS_FUN_NONNULL
+//#define __marlais_counted_by(_field)
 #endif
+#define __marlais_counted_by(_field)
 
 #endif /* !MARLAIS_COMPILER_H */
